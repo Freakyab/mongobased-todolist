9037287088b2b9c824e01a5f2f38599600696cf7
Freakyab <aryanbhisikar1@gmail.com>
2024-08-14 00:24:15 +0530

    chore: Remove unnecessary motion animations in TodoList component

--- app/components/todolist.tsx
+++ app/components/todolist.tsx
@@ -194,13 +194,11 @@ const TodoList = () => {
                 } transition-colors duration-300`}
               />
               {editMode ? (
-                <motion.button
+                <button
                   type="submit"
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
                   className={`bg-red-500 text-white px-4 py-2 rounded-md mt-2 transition-colors duration-300`}>
                   Update Todo
-                </motion.button>
+                </button>
               ) : (
                 <button
                   type="submit"
